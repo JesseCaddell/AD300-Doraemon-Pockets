68a79e717e926c2f3a2aa138cde095b8ac55996a
Jesse Caddell <80575182+JesseCaddell@users.noreply.github.com>
2023-12-12 00:36:33 -0800

    Experimental Branch added

--- src/Main.java
+++ src/Main.java
@@ -51,8 +51,62 @@ class Doraemon implements Storage {
 public class Main {
     public static void main(String[] args) {
         Doraemon doraemon = new Doraemon();
-        //doraemon.addItem("Raygun");
-        //doraemon.removeItem("Raygun");
-        doraemon.printItems();
+
+        // Prepare the table header
+        System.out.printf("%-15s%-15s%-20s%-20s\n", "File Size", "Operation", "Time (ms)", "Memory Usage (MB)");
+        System.out.println("----------------------------------------------------------");
+
+        // Test with 10 items
+        performTest(doraemon, 10);
+
+        // Test with 50 items
+        performTest(doraemon, 50);
+
+        // Test with 1000 items
+        performTest(doraemon, 1000);
+
+        // Test with 50000 items
+        performTest(doraemon, 50000);
+    }
+
+    private static void performTest(Doraemon doraemon, int numItems) {
+        // Add items
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < numItems; i++) {
+            doraemon.addItem("Item" + i);
+        }
+        long endTime = System.currentTimeMillis();
+        long memoryUsageAddition = getUsedMemory();
+
+        // Print results for addition
+        System.out.printf("%-15d%-15s%-20d%-20d\n", numItems, "Addition", (endTime - startTime), memoryUsageAddition);
+
+        // Remove items
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < numItems; i++) {
+            doraemon.removeItem("Item" + i);
+        }
+        endTime = System.currentTimeMillis();
+        long memoryUsageRemoval = getUsedMemory();
+
+        // Print results for removal
+        System.out.printf("%-15d%-15s%-20d%-20d\n", numItems, "Removal", (endTime - startTime), memoryUsageRemoval);
+
+        System.out.println("---------------------------");
+    }
+
+    private static long getUsedMemory() {
+        Runtime runtime = Runtime.getRuntime();
+
+        // Run garbage collector multiple times
+        for (int i = 0; i < 10; i++) {
+            runtime.gc();
+        }
+
+        // Measure memory usage
+        long usedMemoryBefore = runtime.totalMemory() - runtime.freeMemory();
+        long usedMemoryAfter = runtime.totalMemory() - runtime.freeMemory();
+
+        return (usedMemoryAfter - usedMemoryBefore) / (1024 * 1024); // Convert to MB
     }
 }
